0f78516c2ba9f9e261776582de0c03f80259294b
riverrz <shivam123425@gmail.com>
2024-09-14 14:51:45 +0400

    Add config service for environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate as validateEnvs } from '@config/envs/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnvs,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
