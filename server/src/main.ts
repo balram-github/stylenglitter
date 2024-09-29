1a66065cd1bd28626a06102d32b11f3527b36488
riverrz <shivam123425@gmail.com>
2024-09-29 17:10:31 +0400

    Rename loader directory to loaders

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
-import loader from '@loader/app.loader';
+import loader from '@/loaders/app.loader';
 
 async function bootstrap() {
   const logger = new Logger('Main.ts');
