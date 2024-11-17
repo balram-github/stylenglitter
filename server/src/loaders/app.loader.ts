f6235e76fdfdbcf2fe12bf8badbbd0339098757a
riverrz <shivam123425@gmail.com>
2024-11-17 20:22:28 +0400

    Fix cors origin

--- server/src/loaders/app.loader.ts
+++ server/src/loaders/app.loader.ts
@@ -24,8 +24,7 @@ export default function loader(app: INestApplication<any>) {
     origin: [
       'https://www.stylenglitter.com',
       'http://localhost:3000',
-      'https://www.admin.stylenglitter.com',
-      'https://main.d7bw6xdmmxtj1.amplifyapp.com',
+      'https://admin.stylenglitter.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
