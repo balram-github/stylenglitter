cbe7a2872ec5e682411df8a867699fe9677402f0
riverrz <shivam123425@gmail.com>
2025-01-18 10:52:46 +0400

    Improve responsiveness for categories

--- client/src/modules/home/components/categories-list/categories-list.tsx
+++ client/src/modules/home/components/categories-list/categories-list.tsx
@@ -7,7 +7,7 @@ export const CategoriesList = ({ categories }: CategoriesListProps) => {
   return (
     <div className="py-6 md:py-10">
       <h2 className="font-bold text-center text-2xl md:text-4xl">Categories</h2>
-      <div className="flex flex-wrap items-center justify-center gap-8 py-4 md:py-12 w-full">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 py-4 md:py-12 w-full">
         {categories.map((category) => (
           <Link
             href={`/categories/${category.slug}`}
@@ -18,8 +18,8 @@ export const CategoriesList = ({ categories }: CategoriesListProps) => {
               <Image
                 src={category.coverImgUrl}
                 alt={category.slug}
-                width={300}
-                height={300}
+                width={350}
+                height={350}
                 className="hover:scale-125 transition-all duration-300"
               />
             </div>
